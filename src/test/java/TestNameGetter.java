9263e8c1fabf001bd2350d3b9555e0cdc17e82ef
scmurdock <scmurdock@gmail.com>
2025-12-11 06:59:56 -0700

    Initial commit

---
+++ src/test/java/TestNameGetter.java
@@ -0,0 +1,20 @@
+import org.example.NameGetter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestNameGetter {
+
+
+    @DisplayName("Test Greeting")
+    @Test
+    void testReturnGreeting(){
+        NameGetter nameGetter = new NameGetter();
+
+        nameGetter.setName("Sean");
+
+        assertEquals("Hello Sean!", nameGetter.getGreeting());
+
+    }
+}
